fded7e2da3863cef73394262db553c49b6ac526a
Dheeraj N Kashyap <95096072+Dheeraj-02NK@users.noreply.github.com>
2024-01-01 12:59:50 +0530

    Add files via upload

---
+++ 02. Introduction/01. Addition.js
@@ -0,0 +1,12 @@
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  // Example usage
+  var num1 = 5;
+  var num2 = 10;
+  
+  var sum = addNumbers(num1, num2);
+  
+  console.log("The sum is: " + sum);
